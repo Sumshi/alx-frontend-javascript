de916a0e86928545326788fe95d1cdf29b42236a
Sumshi <sumayasomow@gmail.com>
2024-01-03 08:05:27 +0000

    promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,14 +1,14 @@
 function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        const apiData = { message: "API response data" };
-        const success = true;
+  return new Promise((resolve, reject) => {
+    const apiData = { message: 'API response data' };
+    const success = true;
 
-        if (success) {
-            resolve(apiData);
-        } else {
-            reject(new Error("Failed to fetch data from API"));
-        }
-    });
+    if (success) {
+      resolve(apiData);
+    } else {
+      reject(new Error('Failed to fetch data from API'));
+    }
+  });
 }
-
+// export function for external use in 0-main.js
 export default getResponseFromAPI;
