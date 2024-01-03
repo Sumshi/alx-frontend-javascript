9374d66e9f8a92bd1aba4680676df014d45eae8b
Sumshi <sumayasomow@gmail.com>
2024-01-03 16:13:09 +0000

    minor changes

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({ status: 200, body: 'success' }))
-    .catch(() => new Error())
+    .catch(() => new Error())// returns empty error object
     .finally(() => console.log('Got a response from the API'));
 }
