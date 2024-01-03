ce14a0a15c24c5fea6b43a91781bddac60035763
Sumshi <sumayasomow@gmail.com>
2024-01-03 08:26:33 +0000

    fetch api status

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    const apiData = { status: 200, body: 'Success' };
+    // const success = true;
+
+    if (success) {
+      resolve(apiData);
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
+// export function for external use in 0-main.js
+export default getFullResponseFromAPI;
