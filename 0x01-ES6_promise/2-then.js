ba6b87e07d00de3b15c21da714d803ffb99308c7
Sumshi <sumayasomow@gmail.com>
2024-01-03 10:20:38 +0000

    then and catch

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  const response = { status: 200, body: 'Success' };
   return promise
-    .then(() => response)
-    .catch((error) => Error)
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
     .finally(() => console.log('Got a response from the API'));
 }
