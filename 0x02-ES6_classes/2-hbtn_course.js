57737dc6ab89911b0d07ac9235dc90bf6ba9194c
Sumshi <sumayasomow@gmail.com>
2024-01-04 09:40:30 +0000

    setter and getter

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,10 +1,9 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
     // verify attributes during object creation
-    // if (typeof name !== 'string') throw TypeError('Name must be a string');
-    // if (typeof length !== 'number') throw TypeError('Length must be a number');
-    // if (!Array.isArray(students)) throw TypeError('Students must be an array');
-
+    if (typeof name !== 'string' || typeof length !== 'number' || !Array.isArray(students)) {
+      throw new Error('Invalid attribute type');
+    }
     // set private attributes
     this._name = name;
     this._length = length;
