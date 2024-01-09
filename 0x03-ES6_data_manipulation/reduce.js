147f3d31f12dbe1f1423f9c7ee079e9e830e0a46
Sumshi <sumayasomow@gmail.com>
2024-01-09 07:57:57 +0000

    reduce func

--- 0x03-ES6_data_manipulation/reduce.js
+++ 0x03-ES6_data_manipulation/reduce.js
@@ -1,7 +1,11 @@
 // reduce in javascript
 
 const numbers = [1, -1, 2, 3]
+// a = 0, c = 1, sum = 1
+// a = 1, c = -1, sum = 0
+// a = 0, c = 2, sum = 2
+// a = 2, c = 3, sum = 5
 const sum = numbers.reduce((accumulator, currentVal) => {
     return accumulator + currentVal;
-}, 0);
+}, 0);// accumulator is initially set to 0
 console.log(sum);
